a4476cc3a2500a82c30da03b8a7c69c74bbb461f
Danny B <dani_bejan2000@yahoo.com>
2016-10-23 03:01:16 +0300

    Reverting back the minSdk to 15 because of the ChromeCustomTabs error

--- app/src/main/java/ro/adlabs/nofap/Functions.java
+++ app/src/main/java/ro/adlabs/nofap/Functions.java
@@ -4,7 +4,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.NonNull;
 import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsServiceConnection;
 import android.widget.CheckBox;
